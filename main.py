6ce6784d76c77040baa6f8b091405e85a6241689
Abel Ayalew <84567489+nabroleonx@users.noreply.github.com>
2021-09-16 09:16:53 +0300

    Fixed bugs related to directory structure

--- main.py
+++ main.py
@@ -3,19 +3,9 @@ from gooey import Gooey, GooeyParser
 import os
 
 
-def get_file_paths(directory):
-    file_paths = []
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)
-
-    return file_paths
-
-
 def parse_args():
     parser = GooeyParser(description="This program will let you extract zip files or compress files into a zip file")
-    
+
     subparsers = parser.add_subparsers(required=True, dest="process")
     extractor = subparsers.add_parser("Extract", help="Extract zip files")
     compressor = subparsers.add_parser("Compress", help="Compress to zip files")
@@ -23,12 +13,10 @@ def parse_args():
     extractor.add_argument('input_file', widget="FileChooser", help="Choose the file you want to extract")
     compressor.add_argument('input_folder', widget="DirChooser", help="Choose the file you want to Compress")
 
-    extractor.add_argument('output_folder', widget="DirChooser",
-                           help="Extracted file location")
-    compressor.add_argument('output_file', widget="DirChooser",
-                            help="Compressed file location")
+    extractor.add_argument('output_folder', widget="DirChooser", help="Extracted file location")
+    compressor.add_argument('output_file', widget="FileSaver", help="Compressed file location")
 
-    extractor.add_argument('--Password', default=None, help="If you are working with encrypted file, enter the password below")
+    extractor.add_argument('--password', default=None, help="If you are working with encrypted file, enter the password below")
 
     return parser.parse_args()
 
@@ -38,16 +26,18 @@ def main():
     args = parse_args()
 
     if args.process == "Extract":
-        f_name, f_ext = os.path.splitext(args.input_file)
+        dest_folder = os.path.basename(args.input_file).split(".")[0]
         with ZipFile(args.input_file) as output:
-            output.extractall(os.path.join(args.output_folder, f_name), args.Password, pwd=args.Password)
+            output.extractall(os.path.join(args.output_folder, dest_folder), pwd=bytes(str(args.password), "utf-8"))
     else:
-        folder_name = os.path.basename(args.input_folder)
-        file_paths = get_file_paths(args.input_folder)
-
-        with ZipFile(os.path.join(args.output_file, folder_name + ".zip"), 'w') as zipped:
-            for file in file_paths:
-                zipped.write(file)
+        with ZipFile(os.path.join(args.output_file + ".zip"), 'w') as zipped:
+            abs_path = os.path.abspath(args.input_folder)
+            for root, directories, files in os.walk(args.input_folder):
+                for filename in files:
+                    abs_file_name = os.path.abspath(
+                        os.path.join(root, filename))
+                    arcname = abs_file_name[len(abs_path) + 1:]
+                    zipped.write(abs_file_name, arcname)
 
 
 if __name__ == "__main__":
